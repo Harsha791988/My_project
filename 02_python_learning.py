81dc9d185bf4cf12b5d3031043f00cde16690b0d
unknown <raju.shv18@gmail.com>
2026-07-29 16:52:42 +0530

    day 2

---
+++ 02_python_learning.py
@@ -0,0 +1,37 @@
+# name="Harsha"
+# age=30
+# salary=120000
+# is_manager=True
+# print(name)
+# print(type(name))
+# print(age)
+# print(type(age))
+# print(salary)
+# print(type(salary))
+# print(is_manager)
+# print(type(is_manager))
+
+# Arithimetic Operation
+
+# a=20
+# b=3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+# name="harsha "
+# b=10
+
+# print(name * b)
+
+#String operation
+# print("harsha"+ " "+"Raju")
+# manager= None;
+# print(manager)
+# print(type(manager))
+name=int(input("Enter your name: "))
+print(name)
